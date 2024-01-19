0c41d79bd79e27a384f85b074442e6d66cd78404
Carise Fernandez <carise@gmail.com>
2024-01-19 09:24:11 -0800

    Fix test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+import json
 import requests
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
 data = {'url': 'https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Invertebrates/invertebrate_octopus_600x300.jpg'}
+event = {'body': json.dumps(data)}
 
-result = requests.post(url, json=data).json()
+result = requests.post(url, json=event).json()
 print(result)
